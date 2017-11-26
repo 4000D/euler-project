3cbe2ec510d895e4f9169209afa66e0e9386315e
4000D <dkdkajej@gmail.com>
2017-11-26 20:15:33 +0900

    q120 - brute force

---
+++ src/q120-brute.py
@@ -0,0 +1,34 @@
+# Brute Force
+
+import os
+
+def f(a): # sub main func
+    startTime = os.times()[-1]
+
+    _max = a ** 2
+    _ret = 0
+
+    n = 0
+    while True:
+        n += 1
+
+        r1 = ((a-1) ** n) % _max
+        r2 = ((a+1) ** n) % _max
+
+        _ret = max(_ret, (r1 + r2) % _max)
+
+
+        if r1 == r2:
+            endTime = os.times()[-1]
+            print("f(%d) elapsed %.3f sec" % (a, endTime - startTime))
+            return _ret
+
+
+def main():
+    ret = sum([f(a) for a in range(3, 1000 + 1)])
+
+    print("Answer : %d" % ret)
+
+
+if __name__ == '__main__':
+    main()
